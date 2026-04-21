f5fcd77acb9ae6689154b0003399a003e9002b65
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-04-20 21:42:58 -0500

    tema 5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,7 +5,153 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Tema 5
+      h1 Segmentación de mercados
+    .fullwidth-bg.fullwidth-bg--1
+    img(data-aos="fade-up")(src="@/assets/curso/tema5/1.png", alt="").z-2
+    
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text
+          p.mb-0 La segmentación de mercados constituye el punto de partida en la planeación del contenido digital. Permite identificar grupos homogéneos dentro de un mercado amplio, con el propósito de diseñar estrategias diferenciadas y mensajes específicos. En entornos digitales, segmentar no es una opción estratégica sino una condición necesaria para optimizar recursos, aumentar la relevancia del contenido y mejorar la conversión.
+
+    .row.justify-content-center.z-2
+      .col-xl-10
+        .rounded-4.bg-primario-p5
+          .row.justify-content-center.align-items-center
+            .col-lg-9.col-xl-10
+              .p-4
+                p.mb-0 En el contexto del #[em marketing] digital, la segmentación se apoya en datos comportamentales, demográficos y psicográficos que permiten comprender cómo interactúa el usuario en plataformas como Instagram, TikTok, Facebook y LinkedIn. Esta información facilita la personalización del mensaje y la construcción de propuestas de valor alineadas con necesidades reales.
+            .col-lg-3.col-xl-2
+              figure
+                img.w-190(data-aos="fade-down-left", src="@/assets/curso/tema5/2.png", alt="")
+
+    Separador
+    #t_5_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 5.1 Concepto
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-md-10.col-lg-5
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema5/3.png", alt="")
+      .col-lg-7
+        p La segmentación de mercados es el proceso mediante el cual se divide un mercado total en grupos más pequeños de consumidores que comparten características, necesidades o comportamientos similares. Su finalidad es diseñar estrategias específicas para cada grupo, aumentando la efectividad de la comunicación y la eficiencia en la inversión.
+        p.mb-0 Desde una perspectiva estratégica, segmentar implica:
+
+
+    .row.justify-content-center.align-items-center.z-2.mb-5
+      .col-lg-10.col-xl-8
+        img.mb-2.d-sm-none(data-aos="fade-up", src="@/assets/curso/tema5/4-mob.png", alt="Analizar datos del entorno. Identificar patrones de consumo. Clasificar públicos según variables relevantes. Priorizar segmentos con mayor potencial de rentabilidad.")
+        img.mb-2.d-none.d-sm-block(data-aos="fade-up", src="@/assets/curso/tema5/4.png", alt="Analizar datos del entorno. Identificar patrones de consumo. Clasificar públicos según variables relevantes. Priorizar segmentos con mayor potencial de rentabilidad.")
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        .p-4.rounded-4.bg-secundario-s5
+          p.mb-0 En #[em marketing] digital, la segmentación permite que el contenido no sea generalista. Por el contrario, se orienta a públicos definidos, mejorando indicadores como alcance cualificado, interacción y tasa de conversión.
+          
+    Separador
+    #t_5_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 5.2 Técnicas
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-lg-7
+        p Las técnicas de segmentación evolucionaron con la transformación digital. Hoy se combinan métodos tradicionales con análisis de datos provenientes de plataformas digitales y herramientas de analítica.
+        p.mb-0 Entre las principales técni_________________________________________
+          img(data-aos="fade-down-left", src="@/assets/curso/tema5/5.png", alt="")
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-sm-8.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema5/6.png", alt="")
+      .col-lg-9
+        LineaTiempoD.color-secundario
+          p.mb-0(numero="1" titulo="Segmentación demográfica") Clasifica el mercado según edad, género, nivel educativo, ocupación e ingresos. Es útil para definir tono y formato del contenido.
+          p.mb-0(numero="2" titulo="Segmentación geográfica") Divide el mercado según ubicación física. En entornos digitales se aplica a campañas locales o regionales.
+          p.mb-0(numero="3" titulo="Segmentación psicográfica") Considera estilo de vida, intereses, valores y personalidad. Es clave para la construcción de identidad de marca.
+          p.mb-0(numero="4" titulo="Segmentación conductual") Se basa en el comportamiento del usuario, frecuencia de compra, interacción con contenidos, historial de navegación y nivel de fidelidad.
+          p.mb-0(numero="5" titulo="Microsegmentación digital") utiliza algoritmos y datos masivos para crear audiencias específicas. Permite personalizar anuncios y contenidos con alto nivel de precisión.
+        
+    .p-4.rounded-4.bg-primario-p10
+      p.mb-0.text-center Estas técnicas permiten optimizar campañas en entornos SEM, redes sociales y estrategias de contenido orgánico, alineando el mensaje con expectativas concretas del usuario.
+      
+    Separador
+    #t_5_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 5.3 Variables
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema5/7.png", alt="")
+      .col-lg-8
+        p En la segmentación de mercados, las variables corresponden a los criterios o características específicas que permiten clasificar a los consumidores, tales como edad, género, ubicación, intereses o comportamiento digital; mientras que las técnicas hacen referencia al método o enfoque utilizado para organizar esas variables y dividir el mercado en grupos homogéneos, como la segmentación demográfica, geográfica, psicográfica o conductual. En síntesis, las variables representan los datos que describen al público y las técnicas constituyen la forma estructurada de agruparlos estratégicamente para orientar la planeación del contenido digital.
+        p.mb-0 Las principales variables son:
+
+    .row.justify-content-center.mb-5
+      .col-6.col-md-4.col-lg-3.col-xl-2.mb-4.mb-lg-0(data-aos="fade-right")
+        .tarjeta-avatar
+          img.w-90(src='@/assets/curso/tema5/8.png' alt='')
+          .tarjeta.color-primario.shadow
+            .p-3.pt-4
+              .row.justify-content-center
+                .col-auto.text-center
+                  p.mb-0 Demográficas: edad, género, nivel socioeconómico.
+      .col-6.col-md-4.col-lg-3.col-xl-2.mb-4.mb-lg-0(data-aos="fade-left")
+        .tarjeta-avatar
+          img.w-90(src='@/assets/curso/tema5/9.png' alt='')
+          .tarjeta.color-primario.shadow
+            .p-3.pt-4
+              .row.justify-content-center
+                .col-auto.text-center
+                  p.mb-0 Geográficas: país, ciudad, zona urbana o rural.
+      .col-6.col-md-4.col-lg-3.col-xl-2.mb-4.mb-lg-0(data-aos="fade-right")
+        .tarjeta-avatar
+          img.w-90(src='@/assets/curso/tema5/10.png' alt='')
+          .tarjeta.color-primario.shadow
+            .p-3.pt-4
+              .row.justify-content-center
+                .col-auto.text-center
+                  p.mb-0 Psicográficas: intereses, valores, aspiraciones.
+      .col-6.offset-lg-1.offset-xl-0.col-md-4.col-lg-3.col-xl-2.mb-4.mb-lg-0(data-aos="fade-left")
+        .tarjeta-avatar
+          img.w-90(src='@/assets/curso/tema5/11.png' alt='')
+          .tarjeta.color-primario.shadow
+            .p-3.pt-4
+              .row.justify-content-center
+                .col-auto.text-center
+                  p.mb-0 Conductuales: frecuencia de uso digital, interacción con publicaciones, intención de compra.
+      .col-6.col-md-4.col-lg-3.col-xl-2.mb-4.mb-lg-0(data-aos="fade-left")
+        .tarjeta-avatar
+          img.w-90(src='@/assets/curso/tema5/12.png' alt='')
+          .tarjeta.color-primario.shadow
+            .p-3.pt-4
+              .row.justify-content-center
+                .col-auto.text-center
+                  p.mb-0 Tecnológicas: tipo de dispositivo, sistema operativo, horarios de conexión.
+    
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema5/14.png", alt="")
+      .col-lg-8
+        p En entornos digitales, las variables conductuales y tecnológicas adquieren mayor relevancia, ya que permiten comprender cómo y cuándo el usuario consume contenido. Por ejemplo, un segmento que utiliza mayoritariamente dispositivos móviles requerirá formatos verticales y mensajes de impacto inmediato.
+        .p-4.rounded-4.bg-secundario-s5
+          p.mb-0 La segmentación de mercados no es un ejercicio descriptivo sino analítico. Su función es garantizar que la planeación del contenido digital responda a un público claramente definido. Sin segmentación, no hay precisión estratégica; sin precisión, no hay conversión.
+          
+
+    .p-4.rounded-4.bg-terciario-t20
+      .row.justify-content-center.align-items-center
+        .col-xl-1.d-none.d-xl-block
+          figure
+            img(data-aos="fade-down-left", src="@/assets/curso/tema5/13.png", alt="")
+        .col-lg-9
+          p.mb-0 Lo invitamos a visualizar el siguiente video para afianzar los conocimientos de segmentación de mercado y profundizar en cómo se divide un mercado en segmentos relevantes, por qué es crucial para dirigir mensajes adecuados y cómo esto impacta directamente en el éxito de estrategias digitales: 
+        .col-12.col-lg-2
+          a.boton.color-acento-botones.texto-oscuro(href="https://www.youtube.com/watch?v=To0O7JCtAI0", target="_blank")
+            span Ver video
+            i.fas.fa-play-circle
+            
+          
+      
+
 
 
 </template>
